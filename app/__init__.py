ff3431d12e4e13a2bbddecf99cbbe44fbaad2bc7
tangkehu <tangkehu@cecgw.cn>
2019-06-11 16:21:50 +0800

    初始化日志和异常处理

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,14 @@
 
+import os
 import click
-from flask import Flask
+import logging
+from logging.handlers import RotatingFileHandler
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 from config import Config
+from .utils import SSLSMTPHandler
 
 
 db = SQLAlchemy()
@@ -17,6 +21,9 @@ def create_app():
 
     db.init_app(app)
     migrate.init_app(app, db)
+
+    register_logging(app)
+    register_errors(app)
 
     @app.context_processor  # Flask的上下文处理器，向模板的上下文插入新变量（可以是值和函数）
     def inject_context():
@@ -33,3 +40,47 @@ def create_app():
     app.register_blueprint(main_bp)
 
     return app
+
+
+def register_errors(app):
+    @app.errorhandler(403)
+    def forbid_error(e):
+        return render_template('exception.html'), 403
+
+    @app.errorhandler(404)
+    def not_found_error(e):
+        return render_template('exception.html'), 404
+
+    @app.errorhandler(500)
+    def internal_error(e):
+        return render_template('exception.html'), 500
+
+
+def register_logging(app):
+    if not app.debug:
+        if app.config.get('MAIL_SERVER'):
+            mail_handler = SSLSMTPHandler(mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+                                          credentials=(app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD']),
+                                          fromaddr=app.config['MAIL_SENDER'], toaddrs=app.config['MAIL_ADMINS'],
+                                          subject='超级国风WEB小程序 ERRORS 警报日志')
+            mail_handler.setFormatter(logging.Formatter('''
+                Message type:       %(levelname)s
+                Location:           %(pathname)s:%(lineno)d
+                Module:             %(module)s
+                Function:           %(funcName)s
+                Time:               %(asctime)s
+
+                Message:
+
+                %(message)s
+                '''))
+            mail_handler.setLevel(logging.ERROR)
+            app.logger.addHandler(mail_handler)
+
+        if not os.path.exists('logs'):
+            os.mkdir('logs')
+        file_handler = RotatingFileHandler('logs/run.log', maxBytes=10240, backupCount=10)
+        file_handler.setFormatter(logging.Formatter(
+            '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
