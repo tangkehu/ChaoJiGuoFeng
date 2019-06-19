89c9f013ed817d3e6859326b96984292bac63df0
Tang <329937872@qq.com>
2019-06-20 06:57:43 +0800

    二级界面设计

--- app/main/views.py
+++ app/main/views.py
@@ -14,21 +14,21 @@ def news():
     return render_template('main/news.html')
 
 
-# @main_bp.route('/activities')
-# def activities():
-#     return render_template('')
-#
-#
-# @main_bp.route('/works')
-# def works():
-#     return render_template('')
-#
-#
-# @main_bp.route('/products')
-# def products():
-#     return render_template('')
-#
-#
-# @main_bp.route('/about')
-# def about():
-#     return render_template('')
+@main_bp.route('/activities')
+def activities():
+    return render_template('main/activities.html')
+
+
+@main_bp.route('/works')
+def works():
+    return render_template('main/works.html')
+
+
+@main_bp.route('/products')
+def products():
+    return render_template('main/products.html')
+
+
+@main_bp.route('/about')
+def about():
+    return render_template('main/about.html')
