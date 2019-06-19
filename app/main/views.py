947e42fd203325f803ef3f1001604dd3f7d2668a
tangkehu <tangkehu@cecgw.cn>
2019-06-19 17:35:20 +0800

    最新动态页设计

--- app/main/views.py
+++ app/main/views.py
@@ -7,3 +7,28 @@ from . import main_bp
 @main_bp.route('/')
 def index():
     return render_template('main/index.html')
+
+
+@main_bp.route('/news')
+def news():
+    return render_template('main/news.html')
+
+
+# @main_bp.route('/activities')
+# def activities():
+#     return render_template('')
+#
+#
+# @main_bp.route('/works')
+# def works():
+#     return render_template('')
+#
+#
+# @main_bp.route('/products')
+# def products():
+#     return render_template('')
+#
+#
+# @main_bp.route('/about')
+# def about():
+#     return render_template('')
