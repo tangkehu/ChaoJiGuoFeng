c4a7182cdbbd9259850b2ddc40bd34f139575572
Tang <329937872@qq.com>
2019-06-22 18:17:05 +0800

    文章详情页设计完成

--- app/main/views.py
+++ app/main/views.py
@@ -18,11 +18,19 @@ def news():
 @main_bp.route('/news_content/<int:page>')
 def news_content(page):
     time.sleep(1)
-    content_list = [{'img': url_for('static', filename='img/dt.jpg'),
-                     'title': '武侯祠旁读《蜀相》，这一次诵读如此美妙',
-                     'content': '不要以为孩子们静不下来，在精心制作的冥想音乐中，他们如此专心专注，甚至像个小菩萨，内心是安定的，缓缓的告别浮躁，找寻自我心底的宁静。'}]
+    content_list = []
+    for item in range(4):
+        content_list.append({'img': url_for('static', filename='img/dt.jpg'),
+                             'title': '武侯祠旁读《蜀相》，这一次诵读如此美妙',
+                             'info': url_for('.news_info', nid=0),
+                             'content': '不要以为孩子们静不下来，在精心制作的冥想音乐中，他们如此专心专注，甚至像个小菩萨，内心是安定的，缓缓的告别浮躁，找寻自我心底的宁静。'})
     return jsonify({'content': render_template('main/news_content.html', content_list=content_list),
                     'next_url': url_for('.news_content', page=page+1) if page < 5 else ''})
+
+
+@main_bp.route('/news_info/<int:nid>')
+def news_info(nid):
+    return render_template('main/news_info.html')
 
 
 @main_bp.route('/activities')
@@ -33,11 +41,19 @@ def activities():
 @main_bp.route('/activities_content/<int:page>')
 def activities_content(page):
     time.sleep(1)
-    content_list = [{'img': url_for('static', filename='img/hd.jpg'),
-                     'title': '陪伴成长——妈妈的坚持，转移到孩子的坚持',
-                     'content': '亲子陪读有门道——挖掘中华经典的力量系列活动将于6月15日迎来第八期——陪伴成长——妈妈的坚持，转移到孩子的坚持，海光老师将与大家分享影响孩子坚持的几大因素。'}]
+    content_list = []
+    for item in range(4):
+        content_list.append({'img': url_for('static', filename='img/hd.jpg'),
+                             'title': '陪伴成长——妈妈的坚持，转移到孩子的坚持',
+                             'info': url_for('.activities_info', aid=0),
+                             'content': '亲子陪读有门道——挖掘中华经典的力量系列活动将于6月15日迎来第八期——陪伴成长——妈妈的坚持，转移到孩子的坚持，海光老师将与大家分享影响孩子坚持的几大因素。'})
     return jsonify({'content': render_template('main/activities_content.html', content_list=content_list),
                     'next_url': url_for('.activities_content', page=page+1) if page < 5 else ''})
+
+
+@main_bp.route('/activities_info/<int:aid>')
+def activities_info(aid):
+    return render_template('main/activities_info.html')
 
 
 @main_bp.route('/works')
@@ -48,8 +64,10 @@ def works():
 @main_bp.route('/works_content/<int:page>')
 def works_content(page):
     time.sleep(1)
-    content_list = [{'title': '国风作品展示-女生版VLOG展示',
-                     'video': url_for('static', filename='video/zp.mp4')}]
+    content_list = []
+    for item in range(6):
+        content_list.append({'title': '国风作品展示-女生版VLOG展示',
+                             'video': url_for('static', filename='video/zp.mp4')})
     return jsonify({'content': render_template('main/works_content.html', content_list=content_list),
                     'next_url': url_for('.works_content', page=page+1) if page < 5 else ''})
 
@@ -62,9 +80,11 @@ def products():
 @main_bp.route('/products_content/<int:page>')
 def products_content(page):
     time.sleep(1)
-    content_list = [{'img': url_for('static', filename='product/cp.png'),
-                     'name': '经典论语著作，全本，诵读经典，提高诵读能力，提高国风能力',
-                     'price': 99.9}]
+    content_list = []
+    for item in range(8):
+        content_list.append({'img': url_for('static', filename='product/cp.png'),
+                             'name': '经典论语著作，全本，诵读经典，提高诵读能力，提高国风能力',
+                             'price': 99.9})
     return jsonify({'content': render_template('main/products_content.html', content_list=content_list),
                     'next_url': url_for('.products_content', page=page+1) if page < 5 else ''})
 
