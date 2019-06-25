e1ccb952abaa80cb106cd5d052c44416f32a77f8
tangkehu <tangkehu@cecgw.cn>
2019-06-25 17:01:15 +0800

    重新调整后台style样式

--- app/manage/views.py
+++ app/manage/views.py
@@ -6,7 +6,16 @@ from . import manage_bp
 
 @manage_bp.route('/news')
 def news():
-    return render_template('manage/news.html')
+    news_list = []
+    for item in range(20):
+        news_list.append({
+            'id': item + 1,
+            'status': True,
+            'datetime': '2019-06-18 17:00:00',
+            'title': '武侯祠旁读《蜀相》，这一次诵读如此美妙',
+            'content': '不要以为孩子们静不下来，在精心制作的冥想音乐中，他们如此专心专注，甚至像个小菩萨，内心是安定的，缓缓的告别浮躁，找寻自我心底的宁静。'
+        })
+    return render_template('manage/news.html', news_list=news_list)
 
 
 @manage_bp.route('/activities')
