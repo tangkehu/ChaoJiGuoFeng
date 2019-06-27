08b7fb2514e46a568aa4f6b7fe90a241dfc2d3b3
tangkehu <tangkehu@cecgw.cn>
2019-06-27 17:38:00 +0800

    后台basic页面设计

--- app/manage/views.py
+++ app/manage/views.py
@@ -15,7 +15,7 @@ def news():
             'title': '武侯祠旁读《蜀相》，这一次诵读如此美妙',
             'content': '不要以为孩子们静不下来，在精心制作的冥想音乐中，他们如此专心专注，甚至像个小菩萨，内心是安定的，缓缓的告别浮躁，找寻自我心底的宁静。'
         })
-    return render_template('manage/news.html', news_list=news_list)
+    return render_template('manage/news.html', news_list=news_list, page_name='最新动态')
 
 
 @manage_bp.route('/activities')
