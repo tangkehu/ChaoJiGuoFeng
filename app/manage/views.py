b258d3a1cad8b9ac5ae9e0c756bf4c79a628172f
tangkehu <tangkehu@cecgw.cn>
2019-07-02 17:11:10 +0800

    文章编辑界面设计完成

--- app/manage/views.py
+++ app/manage/views.py
@@ -1,6 +1,8 @@
 
-from flask import render_template
+import re
+from flask import render_template, request, redirect, url_for, jsonify
 
+from app.utils import random_filename, resize_img
 from . import manage_bp
 
 
@@ -43,8 +45,18 @@ def about():
     return render_template('manage/news.html')
 
 
-@manage_bp.route('/article')
-@manage_bp.route('/article/<int:tid>')
-@manage_bp.route('/article/<int:tid>/<int:aid>')
+@manage_bp.route('/article/<int:tid>', methods=['GET', 'POST'])
+@manage_bp.route('/article/<int:tid>/<int:aid>', methods=['GET', 'POST'])
 def article(tid=1, aid=0):
+    if request.method == 'POST':
+        print(re.findall(r'src="(.*?)"', request.form.to_dict().get('content')))
+        return redirect(url_for('.news') if tid == 1 else url_for('.activities'))
     return render_template('manage/article.html')
+
+
+@manage_bp.route('/article/upload', methods=['POST'])
+def article_upload():
+    article_file = request.files.get('upload')
+    filepath = 'app/static/article-img/'
+    filename = resize_img(filepath, random_filename(article_file.filename), 600, article_file, True)
+    return jsonify({'uploaded': True, 'url': url_for('static', filename='article-img/'+filename)})
--- app/utils.py
+++ app/utils.py
@@ -1,5 +1,39 @@
 
+import os
+import uuid
+from PIL import Image
 from logging.handlers import SMTPHandler
+
+
+def random_filename(filename):
+    """ 随机文件名 """
+    ext = os.path.splitext(filename)[1]
+    return uuid.uuid4().hex + ext
+
+
+def resize_img(path, filename, size: int, file_obj=None, save_flg=False):
+    """ 根据给定大小对图片进行百分比缩小 """
+    prefix, ext = os.path.splitext(filename)
+    img = Image.open(file_obj) if file_obj else Image.open(os.path.join(path, filename))
+    width = img.size[0]
+    height = img.size[1]
+    if width <= size or height <= size:
+        if save_flg:
+            img.save(os.path.join(path, filename), optimize=True, quality=85)
+        return filename
+    else:
+        if width < height:
+            w_percent = size/float(width)
+            w_size = size
+            h_size = int(height*w_percent)
+        else:
+            h_percent = size/float(height)
+            h_size = size
+            w_size = int(width*h_percent)
+        img = img.resize((w_size, h_size), Image.ANTIALIAS)
+        filename = prefix+'_'+str(size)+ext
+        img.save(os.path.join(path, filename), optimize=True, quality=85)
+        return filename
 
 
 # Provide a class to allow SSL (Not TLS) connection for mail handlers by overloading the emit() method
